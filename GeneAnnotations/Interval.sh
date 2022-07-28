a50fe58b13af1c2c36b98cef67db3c9dbce8a79c
Louie Alexander <82129400+Lalexander27@users.noreply.github.com>
2022-07-28 10:59:58 -0400

    Update Interval.sh

--- GeneAnnotations/Interval.sh
+++ GeneAnnotations/Interval.sh
@@ -6,4 +6,5 @@
 #SBATCH --partition=Orion
 
 module load anaconda3
+#Order as follows: python3 <.py file> <.csv file of intervals> <.gff or .gff3 file of gene features> <annotation file of gene loci and GO terms>
 python3 GenesInInterval.py SignificantIntervals.csv Sbicolor_454_Chr03_gene.gff3 Sbicolor_454_v3_1_1_annotation_info.txt
